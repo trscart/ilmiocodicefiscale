68bc4df09acf27caa8861f28093d4ae7bb3bb0da
trscart <umbertotruscello1998@gmail.com>
2020-09-15 16:34:21 +0200

    adblock blocker

--- src/index.js
+++ src/index.js
@@ -9,6 +9,39 @@ document.addEventListener("DOMContentLoaded", function (event) {
         $("#cf_date-day").append('<option>' + i + '</option>')
     } */
 
+    // Function called if AdBlock is not detected
+    function adBlockNotDetected() {
+        alert('AdBlock is not enabled');
+    }
+    // Function called if AdBlock is detected
+    function adBlockDetected() {
+        alert('AdBlock is enabled');
+    }
+
+    // Recommended audit because AdBlock lock the file 'blockadblock.js' 
+    // If the file is not called, the variable does not exist 'blockAdBlock'
+    // This means that AdBlock is present
+    if (typeof blockAdBlock === 'undefined') {
+        adBlockDetected();
+    } else {
+        blockAdBlock.onDetected(adBlockDetected);
+        blockAdBlock.onNotDetected(adBlockNotDetected);
+        // and|or
+        blockAdBlock.on(true, adBlockDetected);
+        blockAdBlock.on(false, adBlockNotDetected);
+        // and|or
+        blockAdBlock.on(true, adBlockDetected).onNotDetected(adBlockNotDetected);
+    }
+
+    // Change the options
+    blockAdBlock.setOption('checkOnLoad', false);
+    // and|or
+    blockAdBlock.setOption({
+        debug: true,
+        checkOnLoad: false,
+        resetOnEnd: false
+    });
+
     new ClipboardJS('#cf_code');
 
     if (Cookies.get('cf')) {
